48f61d06c0d17d3b7c17d0f5142016aff8d3f492
Jiancheng LI <jc@JianchengdeMacBook-Pro.local>
2019-05-05 00:38:00 +0800

    ParkingLot: test/feat, T3 Unavaiable ParkingLot park a car

--- src/main/java/ParkingLot.java
+++ src/main/java/ParkingLot.java
@@ -1,7 +1,12 @@
 import exception.EmptyCarNumberException;
+import exception.ParkingLotsFullException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ParkingLot {
     private int capacity;
+    private Map<Ticket, Car> lots = new HashMap<>();
 
     public ParkingLot() {
         this.capacity = 0;
@@ -12,8 +17,15 @@ public class ParkingLot {
     }
 
     public Ticket park(Car car){
+        if (!isAvailable()) { throw new ParkingLotsFullException(); }
         if(car.getNumber().equals("")) { throw new EmptyCarNumberException(); }
+
         Ticket ticket = new Ticket(car.getNumber());
+        lots.put(ticket, car);
         return ticket;
+    }
+
+    public boolean isAvailable() {
+        return lots.size() < capacity;
     }
 }
--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,4 +1,5 @@
 import exception.EmptyCarNumberException;
+import exception.ParkingLotsFullException;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,7 +11,7 @@ public class ParkingLotTest {
     public void should_return_ticket_when_parking_given_an_available_parking_lot_and_car_with_number(){
         final String carNumber = "CN123456";
         Car car = new Car(carNumber);
-        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot parkingLot = new ParkingLot(100);
         Ticket ticket = parkingLot.park(car);
 
         Assert.assertNotNull(ticket);
@@ -19,11 +20,26 @@ public class ParkingLotTest {
 
     @Test
     public void should_return_car_number_exception_when_parking_given_an_available_parking_lot_and_car_without_number(){
-        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot parkingLot = new ParkingLot(100);
         Car car = new Car("");
 
         assertThrows(EmptyCarNumberException.class, ()->{
             parkingLot.park(car);
+        });
+    }
+
+    @Test
+    public void should_return_parkinglots_full_exception_when_parking_given_an_unavailable_parkinglots(){
+        ParkingLot parkingLot = new ParkingLot(0);
+        Car car1 = new Car("CN123456");
+
+        assertThrows(ParkingLotsFullException.class, ()->{
+            parkingLot.park(car1);
+        });
+
+        Car car2 = new Car("");
+        assertThrows(ParkingLotsFullException.class, ()->{
+            parkingLot.park(car2);
         });
     }
 }
